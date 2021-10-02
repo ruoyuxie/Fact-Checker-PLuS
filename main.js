cbcc0b3ad9adf36dc8e1870d8d6bc2ef3bf518bf
ruoyuxyz <royx6641@gmail.com>
2021-10-02 15:05:20 -0400

    Roy's code change

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ var contextMenu={
     "contexts":["selection"]
 };
 
-chrome.contextMenus.create(contextMenu);
 
-chrome.contextMenus.onclicked.addListener(function (clickData){
-    if (clickData.menuItemId == "test" && clickData.slectionText){
-        if (typeof clickData.slectionText === 'string' || clickData.slectionText instanceof String) {
-
-            print("this is text")
-        }
-
-    }
-
-})
+// here is the test code for adding options on contextMenu.
+// chrome.contextMenus.create(contextMenu);
+//
+// chrome.contextMenus.onclicked.addListener(function (clickData){
+//     if (clickData.menuItemId == "test" && clickData.slectionText){
+//         if (typeof clickData.slectionText === 'string' || clickData.slectionText instanceof String) {
+//             print("this is text")
+//         }
+//     }
+// })
