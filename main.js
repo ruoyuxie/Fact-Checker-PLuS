ae7171b7e213d7c818ababaed45414d2b8797cae
ruoyuxyz <royx6641@gmail.com>
2021-10-12 20:29:17 -0400

    adding extension bar display

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ var contextMenu={
     "contexts":["selection"]
 };
 
-
 // here is the test code for adding options on contextMenu.
-// chrome.contextMenus.create(contextMenu);
-//
-// chrome.contextMenus.onclicked.addListener(function (clickData){
-//     if (clickData.menuItemId == "test" && clickData.slectionText){
-//         if (typeof clickData.slectionText === 'string' || clickData.slectionText instanceof String) {
-//             print("this is text")
-//         }
-//     }
-// })
+chrome.contextMenus.create(contextMenu);
+
+chrome.contextMenus.onclicked.addListener(function (clickData){
+    if (clickData.menuItemId == "test" && clickData.slectionText){
+        if (typeof clickData.slectionText === 'string' || clickData.slectionText instanceof String) {
+            print("this is text")
+        }
+    }
+})
---
+++ popup.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("#btnsave").click(function () {
+        var name = $("#txt_name").val();
+
+       alert("Hi"+" "+name);
+
+    });
+});
