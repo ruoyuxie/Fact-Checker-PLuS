260b67fc838dee2edaead48690314974617aa040
ryanjunglee1 <47785083+ryanjunglee1@users.noreply.github.com>
2021-10-28 12:44:12 -0400

    remove machine-dependent elements

--- api_requests.py
+++ api_requests.py
@@ -9,14 +9,10 @@ myparams = {
     "key": API_KEY,
     "query": query
 }
-myproxies = {
-    "http": "http://127.0.0.1:50505",
-    "https": "http://127.0.0.1:50505"
-}
 url = 'https://factchecktools.googleapis.com/v1alpha1/claims:search'
 # Make request
 r = requests.get(
-    url, params=myparams, proxies=myproxies, timeout=5)
+    url, params=myparams, timeout=5)
 
 # Store response as a json file
 with open("query-results.json", "w", encoding="utf-8") as f:
