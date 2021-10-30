036c78d7d1ee103a76806c5e3358a15a2852e055
ryanjunglee1 <47785083+ryanjunglee1@users.noreply.github.com>
2021-10-30 16:40:32 -0400

    add files

---
+++ api_request.js
@@ -0,0 +1,41 @@
+// Reads API key from local text file
+async function readAPIkey(file) {
+    try {
+        let resp = await fetch(file)
+            .then(response => response.text())
+            .then(textString => {
+                return textString;
+            });
+        return resp
+    } catch (err) {
+        console.error(err);
+        // Handle errors here
+    }
+    return resp
+}
+
+
+// Send Google API request
+async function getAPIRequest(apiKey) {
+    try {
+        let response = await fetch('https://factchecktools.googleapis.com/v1alpha1/claims:search?' + new URLSearchParams({
+            'key': apiKey,
+            'query': 'Kim Kardashian is married.'
+        }),
+            {
+                method: 'GET',
+                mode: 'no-cors'
+            });
+        return await response.json();
+    } catch (err) {
+        console.error(err);
+        // Handle errors here
+    }
+}
+
+// Get the api key from text file
+var apiKey = readAPIkey("api_key.txt");
+apiKey.then(resp => console.log(resp));
+
+// Query the result
+apiKey.then(resp => console.log(getAPIRequest(resp)))
