74de8d8e729acbecc3d0902bb8e5da8556710a45
ryanjunglee1 <47785083+ryanjunglee1@users.noreply.github.com>
2021-11-06 17:22:56 -0400

    incremental update

--- main.js
+++ main.js
@@ -9,53 +9,51 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create(contextMenu);
 })
 
-chrome.contextMenus.removeAll(function () {
-    chrome.contextMenus.onClicked.addListener(function (clickData) {
-        if (clickData.menuItemId == "test" && clickData.selectionText) {
-            if (typeof clickData.selectionText === 'string' || clickData.selectionText instanceof String) {
-                var unprocessed_text = clickData.selectionText
-                console.log("Unprocessed Text:")
-                console.log(unprocessed_text)
 
-                var unwanted_char = ["/", "\\", "(", ")", "~", "`", "!", "@", "#"
-                    , "$", "%", "^", "&", "*", "_", "-", "=", "+"
-                    , "<", ">", "?", "{", "}", "[", "]", '"', "'"
-                    , "|", ";", ":", ","]
-                var processed_text = unprocessed_text
-                for (var i = 0; i < unwanted_char.length; i++) {
-                    if (unprocessed_text.includes(unwanted_char[i])) {
-                        processed_text = processed_text.replaceAll(unwanted_char[i], "")
-                    }
-                };
-                console.log("Processed Text:")
-                console.log(processed_text)
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+    if (clickData.menuItemId == "test" && clickData.selectionText) {
+        if (typeof clickData.selectionText === 'string' || clickData.selectionText instanceof String) {
+            var unprocessed_text = clickData.selectionText
+            console.log("Unprocessed Text:")
+            console.log(unprocessed_text)
 
-                var unprocessed_sentences = processed_text.split(".")
-                console.log("Unprocessed Sentences:")
-                console.log(unprocessed_sentences)
-
-                var processed_sentences = []
-                var new_sentence
-                for (var i = 0; i < unprocessed_sentences.length; i++) {
-                    if (unprocessed_sentences[i] != "") {
-                        var new_sentence = unprocessed_sentences[i].trim()
-                        processed_sentences.push(new_sentence)
-                    }
+            var unwanted_char = ["/", "\\", "(", ")", "~", "`", "!", "@", "#"
+                , "$", "%", "^", "&", "*", "_", "-", "=", "+"
+                , "<", ">", "?", "{", "}", "[", "]", '"', "'"
+                , "|", ";", ":", ","]
+            var processed_text = unprocessed_text
+            for (var i = 0; i < unwanted_char.length; i++) {
+                if (unprocessed_text.includes(unwanted_char[i])) {
+                    processed_text = processed_text.replaceAll(unwanted_char[i], "")
                 }
+            };
+            console.log("Processed Text:")
+            console.log(processed_text)
 
-                console.log("Processed Sentences:")
-                console.log(processed_sentences)
+            var unprocessed_sentences = processed_text.split(".")
+            console.log("Unprocessed Sentences:")
+            console.log(unprocessed_sentences)
 
-                // API call
-                alarm()
-                queryResults = makeAPIRequest(processed_sentences[0])
-                queryResults.then(results => alarm("It works!"))
-                chrome.tabs.create({ 'url': chrome.extension.getURL('results.html') }, function (tab) {
-                    // Tab opened.
-                });
+            var processed_sentences = []
+            var new_sentence
+            for (var i = 0; i < unprocessed_sentences.length; i++) {
+                if (unprocessed_sentences[i] != "") {
+                    var new_sentence = unprocessed_sentences[i].trim()
+                    processed_sentences.push(new_sentence)
+                }
             }
+
+            console.log("Processed Sentences:")
+            console.log(processed_sentences)
+
+            // API call
+            queryResults = makeAPIRequest(processed_sentences[0])
+            queryResults.then(results => console.log(results))
+            chrome.tabs.create({ 'url': chrome.extension.getURL('results.html') }, function (tab) {
+                // Tab opened.
+            });
         }
-    })
+    }
 });
 
 
